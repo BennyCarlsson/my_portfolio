8fa590b8006c635005ea4f277d181d4629ef7752
Benny <carlsson.benny93@gmail.com>
2016-10-18 19:59:00 +0200

    Click on input div puts input in focus Changed title

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,10 +2,13 @@ import React, {Component} from 'react';
 import FontIcon from 'material-ui/FontIcon';
 
 class SearchBar extends Component{
+  handleClick(){
+    this.refs.searchBar.focus();
+  }
   render(){
     return(
-        <div id="searchBarDiv">
-          <input id="searchBar" type="text"/>
+        <div id="searchBarDiv" onClick={this.handleClick.bind(this)}>
+          <input ref="searchBar" id="searchBar" type="text"/>
           <FontIcon className="material-icons" id="searchBarHamburgerMenuIcon">menu</FontIcon>
         </div>
     );
