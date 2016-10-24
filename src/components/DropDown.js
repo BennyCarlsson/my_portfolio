6cc3be9500f6b1efddfcba7ee2b25b7eebbba4cc
Benny <carlsson.benny93@gmail.com>
2016-10-24 20:34:24 +0200

    added icons to dropdown

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import Paper from 'material-ui/Paper';
 import {List, ListItem} from 'material-ui/List';
+import Person from 'material-ui/svg-icons/social/person';
+import Folder from 'material-ui/svg-icons/file/folder';
+import Description from 'material-ui/svg-icons/action/description';
+import Email from 'material-ui/svg-icons/communication/email';
 
 class DropDown extends Component{
   aboutClick(){
@@ -20,10 +24,10 @@ class DropDown extends Component{
       <div id="DropDownMenuDiv">
       <Paper className="DropDownMenu">
         <List>
-          <ListItem primaryText="About" onClick={this.aboutClick.bind(this)}/>
-          <ListItem primaryText="Projects" onClick={this.projectClick.bind(this)}/>
-          <ListItem primaryText="Lorem Ipsum" onClick={this.loremIpsumClick.bind(this)}/>
-          <ListItem primaryText="Contact" onClick={this.contactClick.bind(this)}/>
+          <ListItem primaryText="About" leftIcon={<Person/>} onClick={this.aboutClick.bind(this)}/>
+          <ListItem primaryText="Projects" leftIcon={<Folder/>} onClick={this.projectClick.bind(this)}/>
+          <ListItem primaryText="Lorem Ipsum" leftIcon={<Description/>} onClick={this.loremIpsumClick.bind(this)}/>
+          <ListItem primaryText="Contact" leftIcon={<Email/>} onClick={this.contactClick.bind(this)}/>
         </List>
       </Paper>
     </div>
