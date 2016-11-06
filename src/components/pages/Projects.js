5ebfbb99e7cadc8c24189bd190867bc35402b0ab
Benny Carlsson <carlsson.benny93@gmail.com>
2016-11-06 17:01:39 +0100

    fixed prjects paper

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -10,8 +10,18 @@ const style = {
   margin: "1%",
   textAlign: 'center',
   display: 'inline-block',
+  marginBottom: "2%"
 };
-
+const titleStyle = {
+  padding:0
+}
+const imageStyleDiv = {
+  height: 136,
+  overflow: "hidden"
+}
+const imageStyle = {
+  width:"100%"
+}
 class Projects extends Component{
   render(){
       return(
@@ -23,12 +33,12 @@ class Projects extends Component{
         </div>
         <div id="projectDiv2" className="projectDiv">
           <Paper style={style} zDepth={3} className="projectPaperDiv">
-            <FollowMeHomeCard/>
+            <BaraGodaNyheter/>
           </Paper>
         </div>
         <div id="projectDiv3" className="projectDiv">
           <Paper style={style} zDepth={3} className="projectPaperDiv">
-            <FollowMeHomeCard/>
+            <Styggs/>
           </Paper>
         </div>
         <div id="projectDiv4" className="projectDiv">
@@ -55,24 +65,65 @@ class FollowMeHomeCard extends Component{
     return(
       <Card>
         <CardMedia
-          overlay={<CardTitle title="Follow Me Home" subtitle="Android app" />}
+          overlay={<CardTitle style={titleStyle} title="Follow Me Home"/>}
         >
-          <img src="images/foljmejhem.png" />
+          <div className="paperImageClass" style={imageStyleDiv}>
+            <img src="images/foljmejhem.png" style={imageStyle}/>
+          </div>
         </CardMedia>
         <CardText>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+          Android application made 2015 as a school project. Use the gps in your phone to make sure you and your friend gets safely home!
         </CardText>
         <CardActions>
-        <IconButton>
-          <ActionHome />
-        </IconButton>
-
-          <a href="https://github.com/BennyCarlsson/my_portfolio"><i className="fa fa-github" aria-hidden="true"></i></a>
-
+          <a href="https://github.com/BennyCarlsson/my_portfolio" target="_blank"><i className="fa fa-github" aria-hidden="true"></i> Github</a>
+          <a href="http://bennycarlsson.com/foljmejhem/" target="_blank"><i className="fa fa-globe" aria-hidden="true"></i> Website</a>
         </CardActions>
       </Card>
     )
   }
 }
+class BaraGodaNyheter extends Component{
+  render(){
+    return(
+      <Card>
+        <CardMedia
+          overlay={<CardTitle style={titleStyle} title="Bara Goda Nyheter"/>}
+        >
+          <div className="paperImageClass" style={imageStyleDiv}>
+            <img src="images/baragodanyheter.png" style={imageStyle}/>
+          </div>
+        </CardMedia>
+        <CardText>
+          Android application made 2015 as a school project. Use the gps in your phone to make sure you and your friend gets safely home!
+        </CardText>
+        <CardActions>
+          <a href="https://github.com/BennyCarlsson/my_portfolio" target="_blank"><i className="fa fa-github" aria-hidden="true"></i> Github</a>
+          <a href="http://bennycarlsson.com/foljmejhem/" target="_blank"><i className="fa fa-globe" aria-hidden="true"></i> Website</a>
+        </CardActions>
+      </Card>
+    )
+  }
+}
+  class Styggs extends Component{
+    render(){
+      return(
+        <Card>
+          <CardMedia
+            overlay={<CardTitle style={titleStyle} title="STYGGS"/>}
+          >
+            <div className="paperImageClass" style={imageStyleDiv}>
+              <img src="images/STYGGS.png" style={imageStyle}/>
+            </div>
+          </CardMedia>
+          <CardText>
+            Android application made 2015 as a school project. Use the gps in your phone to make sure you and your friend gets safely home!
+          </CardText>
+          <CardActions>
+            <a href="https://github.com/BennyCarlsson/my_portfolio" target="_blank"><i className="fa fa-github" aria-hidden="true"></i> Github</a>
+            <a href="http://bennycarlsson.com/foljmejhem/" target="_blank"><i className="fa fa-globe" aria-hidden="true"></i> Website</a>
+          </CardActions>
+        </Card>
+      )
+    }
+  }
 export default Projects;
