bf218dc88e38258c75cade264f7c43a28f697de7
Benny Carlsson <carlsson.benny93@gmail.com>
2016-11-21 21:01:49 +0100

    started on using tabs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import baseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import baseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import {GridList, GridTile} from 'material-ui/GridList';
 import Menu from './components/Menu.js';
--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -5,6 +5,22 @@ import Person from 'material-ui/svg-icons/social/person';
 import Folder from 'material-ui/svg-icons/file/folder';
 import Description from 'material-ui/svg-icons/action/description';
 import Email from 'material-ui/svg-icons/communication/email';
+import {Tabs, Tab} from 'material-ui/Tabs';
+
+const styles = {
+  headline: {
+    fontSize: 24,
+    paddingTop: 16,
+    marginBottom: 12,
+    fontWeight: 400,
+  },
+  tab: {
+      fontSize: 24,
+  },
+  tabs: {
+    backgroundColor:"#00838F",
+  }
+}
 
 class DropDown extends Component{
   aboutClick(){
@@ -19,13 +35,35 @@ class DropDown extends Component{
   render() {
     return (
       <div id="DropDownMenuDiv">
-      <Paper className="DropDownMenu">
-        <List>
-          <ListItem primaryText="About" leftIcon={<Person/>} onClick={this.aboutClick.bind(this)}/>
-          <ListItem primaryText="Projects" leftIcon={<Folder/>} onClick={this.projectClick.bind(this)}/>
-          <ListItem primaryText="Contact" leftIcon={<Email/>} onClick={this.contactClick.bind(this)}/>
-        </List>
-      </Paper>
+  <Tabs inkBarStyle={{backgroundColor: 'red'}} className="tabs">
+    <Tab style={styles.tab} icon={<Person/>} label="ABOUT">
+      <div>
+        <h2 style={styles.headline}>Tab One</h2>
+        <p>
+          This is an example tab.
+        </p>
+        <p>
+          You can put any sort of HTML or react component in here. It even keeps the component state!
+        </p>
+      </div>
+    </Tab>
+    <Tab style={styles.tab} icon={<Folder/>} label="PROJECTS">
+      <div>
+        <h2 style={styles.headline}>Tab Two</h2>
+        <p>
+          This is another example tab.
+        </p>
+      </div>
+    </Tab>
+    <Tab style={styles.tab} icon={<Email/>} label="CONTACT">
+      <div>
+        <h2 style={styles.headline}>Tab Three</h2>
+        <p>
+          This is a third example tab.
+        </p>
+      </div>
+    </Tab>
+  </Tabs>
     </div>
     );
   }
