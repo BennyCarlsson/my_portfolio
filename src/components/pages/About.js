374221c6e83a897b8e42fa554c6ead6208c42140
Benny Carlsson <carlsson.benny93@gmail.com>
2017-05-07 14:22:07 +0200

    fixed google analytics and spelling errors

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -36,8 +36,8 @@ class About extends Component{
               <p>Greetings, my name is Bennny Carlsson and
               I was born about {this.state.secondsSinceMyBirth} seconds ago (~{this.state.yearsSinceMyBirth}years )
               in a small town called Nässjö in Sweden. I love programming and everything that has to do with technology, the internet and writing code.
-              Moved to Kalmar 2012 to studie software development at Linnaeus University and now I live in small town close to Växjö working full time writing java. During my free time I try to spend as
-              much time possible writing code and other cool stuff to show of on the internet.</p>
+              Moved to Kalmar 2012 to study software development at Linnaeus University and now I live in small town close to Växjö working full time writing java. During my free time I try to spend as
+              much time possible writing code and other cool stuff to show off on the internet.</p>
 
               <Chips/>
               <SocialMediaPhone/>
